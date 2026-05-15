3d25348d4123a9ae162b9d1cdd88ff937febad27
Vincent Tumminello <vincent.tumminello@uon.edu.au>
2026-05-15 12:27:52 +1000

    Change default iterations to 9000 for shorter testing

--- src/mjlab/tasks/velocity/config/nugus/rl_cfg.py
+++ src/mjlab/tasks/velocity/config/nugus/rl_cfg.py
@@ -42,5 +42,5 @@ def nubots_nugus_ppo_runner_cfg() -> RslRlOnPolicyRunnerCfg:
     experiment_name="nugus_velocity",
     save_interval=500,
     num_steps_per_env=24,
-    max_iterations=45_000,
+    max_iterations=9_000,
   )
